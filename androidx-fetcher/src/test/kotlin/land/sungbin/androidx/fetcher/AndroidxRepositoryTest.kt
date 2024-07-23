d481a6077b01b07b2250b317d8bf14ab49569062
jisungbin <ji@sungb.in>
2024-07-23 14:56:35 +0900

    Rename test functions
    camel case -> snake case

--- androidx-fetcher/src/test/kotlin/land/sungbin/androidx/fetcher/AndroidxRepositoryTest.kt
+++ androidx-fetcher/src/test/kotlin/land/sungbin/androidx/fetcher/AndroidxRepositoryTest.kt
@@ -1,29 +1,49 @@
 package land.sungbin.androidx.fetcher
 
+import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import mockwebserver3.MockWebServer
+import mockwebserver3.junit5.internal.MockWebServerExtension
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(MockWebServerExtension::class)
 class AndroidxRepositoryTest {
-  @Test fun givenRemoteLoggingContextWithLevelAboveNoneWhenFetchingEnableHttpLogging() {
+  private lateinit var logger: TestLogger
+  private lateinit var server: MockWebServer
+  private lateinit var repo: AndroidxRepository
+
+  @BeforeTest fun prepare(server: MockWebServer) {
+    logger = TestLogger()
+    this.server = server
+    repo = AndroidxRepository(server.url("/"), logger, UnconfinedTestDispatcher())
+  }
+
+  @Test fun given_remoteLoggingContext_with_level_above_none_when_fetching_enable_http_logging() {
 
   }
 
-  @Test fun givenRemoteLoggingContextWithEventLoggingWhenFetchingEnableEventLogging() {
+  @Test fun given_remoteLoggingContext_with_level_none_when_fetching_disable_http_logging() {
 
   }
 
-  @Test fun givenRemoteLoggingContextWithBothLogsEnabledWhenFetchingEnableHttpAndEventLogging() {
+  @Test fun given_remoteLoggingContext_with_event_logging_when_fetching_enable_event_logging() {
 
   }
 
-  @Test fun givenRemoteCachingContextWhenFetchingApplyHttpCache() {
+  @Test fun given_remoteLoggingContext_with_both_logs_enabled_when_fetching_enable_http_and_event_logging() {
 
   }
 
-  @Test fun givenGitHubAuthorizationContextWhenFetchingAddAuthorizationHeader() {
+  @Test fun given_remoteCachingContext_when_fetching_apply_http_cache() {
 
   }
 
-  @Test fun givenApiResponseIsNotSuccessfulWhenFetchingLogError() {
+  @Test fun given_githubAuthorizationContext_when_fetching_add_authorization_header() {
+
+  }
+
+  @Test fun given_api_response_is_not_successful_when_fetching_log_error() {
 
   }
 }
